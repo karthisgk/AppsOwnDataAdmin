a0009953acef0c20cac612922dbf4e5532fc60bc
karthick-zoftsolutions <karthick@zoftsolutions.com>
2023-05-03 18:44:39 +0530

    git initial commit

---
+++ AppOwnsDataShared/Migrations/20230419152744_Initial-Create.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppOwnsDataShared.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActivityLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LoginId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Activity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tenant = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkspaceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dataset = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatasetId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Report = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReportId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReportType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PageName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OriginalReportId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LoadDuration = table.Column<int>(type: "int", nullable: true),
+                    RenderDuration = table.Column<int>(type: "int", nullable: true),
+                    CorrelationId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmbedTokenId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityLog", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProfileId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkspaceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkspaceUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatabaseServer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatabaseName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatabaseUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DatabaseUserPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    LoginId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CanEdit = table.Column<bool>(type: "bit", nullable: false),
+                    CanCreate = table.Column<bool>(type: "bit", nullable: false),
+                    TenantAdmin = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastLogin = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TenantName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.LoginId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityLog");
+
+            migrationBuilder.DropTable(
+                name: "Tenants");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
